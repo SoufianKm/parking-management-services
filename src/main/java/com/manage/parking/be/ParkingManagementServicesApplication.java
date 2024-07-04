af9a8981adb55e4b7ad45cf9ca8d34ef3bddcdb6
soufiankamma <soufian.kamma@gmail.com>
2024-07-04 17:35:37 +0100

    Initial commit

---
+++ src/main/java/com/manage/parking/be/ParkingManagementServicesApplication.java
@@ -0,0 +1,13 @@
+package com.manage.parking.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manage/parking/be/ParkingManagementServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.parking.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
